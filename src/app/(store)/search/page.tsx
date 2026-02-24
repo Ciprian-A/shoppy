80e6c500fceb2e425a596948ea6f493b2ee45603
Ciprian-A <ciprianoffice10@gmail.com>
2026-02-24 00:26:55 +0000

    update ui for search result

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -35,9 +35,9 @@ const SearchPage = async ({
 		)
 	}
 	return (
-		<div className='flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4'>
-			<div className='bg-white p-8 rounded-lg shadow-md w-full max-w-4xl'>
-				<h1 className='text-3-xl font-bold mb-6 text-center'>
+		<div className='flex flex-col items-center justify-top w-full min-h-screen bg-gray-100 '>
+			<div className='bg-white p-8 rounded-lg shadow-md w-full min-h-screen '>
+				<h1 className='text-2xl font-bold mb-6 '>
 					Search results for: {query}
 				</h1>
 				<ProductGrid products={mappedProducts} />
